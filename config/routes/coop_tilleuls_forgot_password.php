0e851dc0d756442bf127bc8ad51b79d129dd533a
Piotr Rugala <piotr@isedo.pl>
2025-08-16 15:47:09 +0700

    Update forgot password route to remove trailing slash and add POST reset endpoint

--- config/routes/coop_tilleuls_forgot_password.php
+++ config/routes/coop_tilleuls_forgot_password.php
@@ -6,5 +6,13 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
     $routingConfigurator->import('@CoopTilleulsForgotPasswordBundle/Resources/config/routing.xml')
-        ->prefix('/api/auth/forgot-password');
+        ->prefix('/api/auth/forgot-password')
+        ->options([
+            'trailing_slash_on_root' => false,
+        ]);
+    $routingConfigurator->add('coop_tilleuls_forgot_password.reset', '/api/auth/forgot-password')
+        ->controller('coop_tilleuls_forgot_password.controller.reset_password')
+        ->methods([
+            'POST',
+        ]);
 };
