e55f97b6237c43bfacc08123bea8022687ec3f5e
Piotr Rugala <piotr@isedo.pl>
2025-08-16 15:48:16 +0700

    Remove trailing slash option from forgot password route configuration

--- config/routes/coop_tilleuls_forgot_password.php
+++ config/routes/coop_tilleuls_forgot_password.php
@@ -6,10 +6,7 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routingConfigurator): void {
     $routingConfigurator->import('@CoopTilleulsForgotPasswordBundle/Resources/config/routing.xml')
-        ->prefix('/api/auth/forgot-password')
-        ->options([
-            'trailing_slash_on_root' => false,
-        ]);
+        ->prefix('/api/auth/forgot-password');
     $routingConfigurator->add('coop_tilleuls_forgot_password.reset', '/api/auth/forgot-password')
         ->controller('coop_tilleuls_forgot_password.controller.reset_password')
         ->methods([
